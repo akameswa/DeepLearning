fd2884711988a87e40da4d17dcbea2cfc6040aa8
akameswa <adithya.k.nitt@gmail.com>
2024-02-11 10:44:28 -0500

    update mytorch from hw1p1

--- hw2/p1/mytorch/nn/linear.py
+++ hw2/p1/mytorch/nn/linear.py
@@ -1,46 +1,41 @@
 import numpy as np
 
-# Copy your Linear class from HW1P1 here
+
 class Linear:
 
     def __init__(self, in_features, out_features, debug=False):
-
-        self.W = np.zeros((out_features, in_features), dtype="f")
-        self.b = np.zeros((out_features, 1), dtype="f")
-        self.dLdW = np.zeros((out_features, in_features), dtype="f")
-        self.dLdb = np.zeros((out_features, 1), dtype="f")
+        """
+        Initialize the weights and biases with zeros
+        Checkout np.zeros function.
+        Read the writeup to identify the right shapes for all.
+        """
+        self.W = np.zeros((out_features, in_features))
+        self.b = np.zeros((out_features, 1))
 
         self.debug = debug
 
     def forward(self, A):
-
+        """
+        :param A: Input to the linear layer with shape (N, C0)
+        :return: Output Z of linear layer with shape (N, C1)
+        Read the writeup for implementation details
+        """
         self.A = A
         self.N = A.shape[0]
-        self.Ones = np.ones((self.N, 1), dtype="f")
-        Z = None  # TODO
+        # Think how will self.Ones helps in the calculations and uncomment below
+        self.Ones = np.ones((self.N,1))
+        Z = A @ (self.W).T + self.Ones @ (self.b).T
 
-        return NotImplemented
+        return Z
 
     def backward(self, dLdZ):
 
-        dZdA = None  # TODO
-        dZdW = None  # TODO
-        dZdi = None
-        dZdb = None  # TODO
-        dLdA = None  # TODO
-        dLdW = None  # TODO
-        dLdi = None
-        dLdb = None  # TODO
-        self.dLdW = dLdW / self.N
-        self.dLdb = dLdb / self.N
+        dLdA = dLdZ @ self.W
+        self.dLdW = dLdZ.T @ self.A
+        self.dLdb = dLdZ.T @ self.Ones
 
         if self.debug:
-
-            self.dZdA = dZdA
-            self.dZdW = dZdW
-            self.dZdi = dZdi
-            self.dZdb = dZdb
+            
             self.dLdA = dLdA
-            self.dLdi = dLdi
 
-        return NotImplemented
+        return dLdA
--- hw2/p1/mytorch/nn/loss.py
+++ hw2/p1/mytorch/nn/loss.py
@@ -1,47 +1,63 @@
 import numpy as np
 
-# Copy your Linear class from HW1P1 here
+
 class MSELoss:
 
     def forward(self, A, Y):
+        """
+        Calculate the Mean Squared error
+        :param A: Output of the model of shape (N, C)
+        :param Y: Ground-truth values of shape (N, C)
+        :Return: MSE Loss(scalar)
+
+        """
 
         self.A = A
         self.Y = Y
-        N = A.shape[0]
-        C = A.shape[1]
-        se = None  # TODO
-        sse = None  # TODO
-        mse = sse / (N * C)
+        self.N = A.shape[0]
+        self.C = A.shape[1]
+        se = (A - Y) * (A - Y)
+        sse = np.ones((self.N,1)).T @ se @ np.ones((self.C,1))
+        mse = sse / (self.N * self.C)
 
-        return NotImplemented
+        return mse
 
     def backward(self):
 
-        dLdA = None
+        dLdA = 2 * (self.A - self.Y) / (self.N * self.C)
 
-        return NotImplemented
+        return dLdA
 
 
 class CrossEntropyLoss:
 
     def forward(self, A, Y):
+        """
+        Calculate the Cross Entropy Loss
+        :param A: Output of the model of shape (N, C)
+        :param Y: Ground-truth values of shape (N, C)
+        :Return: CrossEntropyLoss(scalar)
 
+        Refer the the writeup to determine the shapes of all the variables.
+        Use dtype ='f' whenever initializing with np.zeros()
+        """
         self.A = A
         self.Y = Y
         N = A.shape[0]
         C = A.shape[1]
-        Ones_C = np.ones((C, 1), dtype="f")
-        Ones_N = np.ones((N, 1), dtype="f")
 
-        self.softmax = None  # TODO
-        crossentropy = None  # TODO
-        sum_crossentropy = None  # TODO
+        Ones_C = np.ones((C,1))
+        Ones_N = np.ones((N,1))
+
+        self.softmax = np.exp(A) / np.sum(np.exp(A), axis=1, keepdims=True)
+        crossentropy = ((-Y)*np.log(self.softmax)) @ Ones_C
+        sum_crossentropy = Ones_N.T @ crossentropy
         L = sum_crossentropy / N
 
-        return NotImplemented
+        return L
 
     def backward(self):
 
-        dLdA = None  # TODO
+        dLdA = (self.softmax - self.Y)/self.A.shape[0]
 
-        return NotImplemented
+        return dLdA
