4feb76d725c6dba147d5b8d6b7e2419a7703d59c
akameswa <adithya.k.nitt@gmail.com>
2024-03-29 13:09:01 -0400

    hw3p1: complete CTCDecoding

--- hw3/p1/CTC/CTCDecoding.py
+++ hw3/p1/CTC/CTCDecoding.py
@@ -52,9 +52,25 @@ class GreedySearchDecoder(object):
         # 4. Select most probable symbol and append to decoded_path
         # 5. Compress sequence (Inside or outside the loop)
 
-        #return decoded_path, path_prob
-        raise NotImplementedError
+        last_symbol = None
 
+        for i in range(len(y_probs[0])):
+            max_prob = 0
+            for j in range(len(y_probs)):
+                prob = y_probs[j][i]
+                if prob > max_prob:
+                    max_prob = prob
+                    max_symbol = j
+
+            path_prob *= max_prob
+
+            if max_symbol != blank and (last_symbol != self.symbol_set[max_symbol - 1]):
+                decoded_path.append(self.symbol_set[max_symbol - 1])
+                
+            last_symbol = self.symbol_set[max_symbol - 1]
+
+        decoded_path = ''.join(decoded_path)
+        return decoded_path, path_prob
 
 class BeamSearchDecoder(object):
 
@@ -99,10 +115,114 @@ class BeamSearchDecoder(object):
             all the final merged paths with their scores
 
         """
-
         T = y_probs.shape[1]
-        bestPath, FinalPathScore = None, None
+
+        NewPathsWithTerminalBlank, NewPathsWithTerminalSymbol, NewBlankPathScore, NewPathScore = self.InitializePaths(self.symbol_set, y_probs[:, 0, :])
+
+        for t in range(1, T):
+            PathsWithTerminalBlank, PathsWithTerminalSymbol, BlankPathScore, PathScore = self.Prune(NewPathsWithTerminalBlank, NewPathsWithTerminalSymbol, NewBlankPathScore, NewPathScore, self.beam_width)
+            NewPathsWithTerminalBlank, NewBlankPathScore = self.ExtendWithBlank(PathsWithTerminalBlank, PathsWithTerminalSymbol, y_probs[:, t, :], BlankPathScore, PathScore)
+            NewPathsWithTerminalSymbol, NewPathScore = self.ExtendWithSymbol(PathsWithTerminalBlank, PathsWithTerminalSymbol, self.symbol_set, y_probs[:, t, :], BlankPathScore, PathScore)
+
+        _, FinalPathScore = self.MergeIdenticalPaths(NewPathsWithTerminalBlank, NewPathsWithTerminalSymbol, NewBlankPathScore, NewPathScore)
+
+        bestPath = max(FinalPathScore, key=lambda path: FinalPathScore[path])
+        return bestPath, FinalPathScore
+
+
+    def InitializePaths(self, SymbolSet, y):
+        InitialBlankPathScore = {}
+        InitialPathScore = {}
+
+        path = ""
+        InitialBlankPathScore[path] = y[0]
+        InitialPathsWithFinalBlank = []
+        InitialPathsWithFinalBlank.append(path)
+
+        InitialPathWithFinalSymbol = []
+
+        for i, c in enumerate(SymbolSet):
+            path = c
+            InitialPathScore[path] = y[i + 1]
+            InitialPathWithFinalSymbol.append(path)
+
+        return InitialPathsWithFinalBlank, InitialPathWithFinalSymbol, InitialBlankPathScore, InitialPathScore
+
+
+    def ExtendWithBlank(self, PathsWithTerminalBlank, PathsWithTerminalSymbol, y, BlankPathScore, PathScore):
+        UpdatedPathsWithTerminalBlank = []
+        UpdatedBlankPathScore = {}
+
+        for path in PathsWithTerminalBlank:
+            UpdatedPathsWithTerminalBlank.append(path)
+            UpdatedBlankPathScore[path] = BlankPathScore[path] * y[0]
+
+        for path in PathsWithTerminalSymbol:
+            if path in UpdatedPathsWithTerminalBlank:
+                UpdatedBlankPathScore[path] += PathScore[path] * y[0]
+            else:
+                UpdatedPathsWithTerminalBlank.append(path)
+                UpdatedBlankPathScore[path] = PathScore[path] * y[0]
+
+        return UpdatedPathsWithTerminalBlank, UpdatedBlankPathScore
+
+    def ExtendWithSymbol(self, PathsWithTerminalBlank, PathsWithTerminalSymbol, SymbolSet, y, BlankPathScore, PathScore):
+        UpdatedPathsWithTerminalSymbol = set()
+        UpdatedPathScore = {}
+
+        for path in PathsWithTerminalBlank:
+            for i, c in enumerate(SymbolSet):
+                newPath = path + c
+                UpdatedPathsWithTerminalSymbol.add(newPath)
+                UpdatedPathScore[newPath] = BlankPathScore[path] * y[i + 1]
+
+        for path in PathsWithTerminalSymbol:
+            for i, c in enumerate(SymbolSet):
+                newPath = path if (c == path[-1]) else path + c
+                if newPath in UpdatedPathsWithTerminalSymbol:
+                    UpdatedPathScore[newPath] += PathScore[path] * y[i + 1]
+                else:
+                    UpdatedPathsWithTerminalSymbol.add(newPath)
+                    UpdatedPathScore[newPath] = PathScore[path] * y[i + 1]
+
+        return UpdatedPathsWithTerminalSymbol, UpdatedPathScore
+
+    def Prune(self, PathsWithTerminalBlank, PathsWithTerminalSymbol, BlankPathScore, PathScore, BeamWidth):
+        PrunedBlankPathScore = {}
+        PrunedPathScore = {}
         
-        
-        #return bestPath, FinalPathScore
-        raise NotImplementedError
+        scorelist = []
+        for p in PathsWithTerminalBlank:
+            scorelist.append(BlankPathScore[p])
+        for p in PathsWithTerminalSymbol:
+            scorelist.append(PathScore[p])
+
+        scorelist.sort(reverse=True)
+        cutoff = scorelist[BeamWidth] if (BeamWidth < len(scorelist)) else scorelist[-1]
+
+        PrunedPathsWithTerminalBlank = []
+        for p in PathsWithTerminalBlank:
+            if BlankPathScore[p] > cutoff:
+                PrunedPathsWithTerminalBlank.append(p)
+                PrunedBlankPathScore[p] = BlankPathScore[p]
+                            
+        PrunedPathsWithTerminalSymbol = []
+        for p in PathsWithTerminalSymbol:
+            if PathScore[p] > cutoff:
+                PrunedPathsWithTerminalSymbol.append(p)
+                PrunedPathScore[p] = PathScore[p]
+
+        return PrunedPathsWithTerminalBlank, PrunedPathsWithTerminalSymbol, PrunedBlankPathScore, PrunedPathScore
+
+    def MergeIdenticalPaths(self, PathsWithTerminalBlank, PathsWithTerminalSymbol, BlankPathScore, PathScore):
+        MergedPaths = PathsWithTerminalSymbol
+        FinalPathScore = PathScore
+
+        for p in PathsWithTerminalBlank:
+            if p in MergedPaths:
+                FinalPathScore[p] += BlankPathScore[p]
+            else:
+                MergedPaths.add(p)
+                FinalPathScore[p] = BlankPathScore[p]
+                
+        return MergedPaths, FinalPathScore
